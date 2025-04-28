bdb3e799bc40593e24623f871e4fb9f73e0abfbf
Nicolas <123176725+NicolasMauclair@users.noreply.github.com>
2025-04-28 19:25:43 +0200

    ✨ création entité, repository, service et endpoint

---
+++ src/main/java/fr/nicolas/trikount/trikount_backend/controller/userController.java
@@ -0,0 +1,35 @@
+package fr.nicolas.trikount.trikount_backend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import fr.nicolas.trikount.trikount_backend.model.users;
+import fr.nicolas.trikount.trikount_backend.service.UserService;
+
+@RestController
+@RequestMapping("/api/users")
+public class userController {
+
+  @Autowired
+  private UserService userService;
+
+  @GetMapping("/bonjour")
+    public String sayHello() {
+        return "Bonjour";
+    }
+
+  @GetMapping("/{username}")
+  public ResponseEntity<users> getUser(@PathVariable String username) {
+    users user = userService.getUserByUsername(username);
+    if (user != null) {
+      return new ResponseEntity<>(user, HttpStatus.OK);
+    } else {
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+  }
+}
