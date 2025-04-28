fba69631ecfcf9e1ab68c3165ac933e1c55c2533
Nicolas <123176725+NicolasMauclair@users.noreply.github.com>
2025-04-28 17:39:10 +0200

    first commit

---
+++ src/test/java/fr/nicolas/trikount/trikount_backend/TrikountApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.nicolas.trikount.trikount_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrikountApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
